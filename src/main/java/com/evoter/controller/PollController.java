857747f12b2a11d6a5cb7ed4122bf68bea5e6a0a
Dotson-Show <showunmioludotun@gmail.com>
2023-02-27 00:56:01 +0100

    Makes Rest controllers

--- src/main/java/com/evoter/controller/PollController.java
+++ src/main/java/com/evoter/controller/PollController.java
@@ -1,4 +1,12 @@
 package com.evoter.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author showunmioludotun
+ */
+@RestController
+@RequestMapping("/api/v1")
 public class PollController {
 }
--- src/main/java/com/evoter/controller/UserController.java
+++ src/main/java/com/evoter/controller/UserController.java
@@ -2,7 +2,6 @@ package com.evoter.controller;
 
 import com.evoter.entity.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,6 @@ import java.util.List;
 /**
  * @author showunmioludotun
  */
-@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
